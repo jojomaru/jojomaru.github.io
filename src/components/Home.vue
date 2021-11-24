0c504c42d2bbe135393a7848f8f3b2a209c51d73
jojomaru <kachinkmanworld@gmail.com>
2021-11-24 21:51:43 +0800

    remove links

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,10 +5,6 @@
       <b>And this is my portfolio. Check out my projects down below!</b>
     </p>
     <h3>In progress</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
   </div>
 </template>
 
