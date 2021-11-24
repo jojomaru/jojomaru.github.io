4228a74dbcfe00b395d3efe8f9d14d062bf433b0
jojomaru <kachinkmanworld@gmail.com>
2021-11-24 21:23:30 +0800

    fix weird removal of files

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/img/head.png">
+  <Home msg="Hi, I'm Thomas Tai!"/>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
